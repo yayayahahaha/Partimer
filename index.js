677fdcd5ed3a41da3f4026e43ae6b917d06df2a1
flyc <yayayahahahaooii@gmail.com>
2025-06-29 00:05:20 +0800

    update and prune

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { beforeStart, displayMousePosition } from './utils/others.js'
 import { extract, market, money } from './utils/money-utils.js'
-import { redRobot, spring, spring2, 奧迪溫雅努斯, 實驗室 } from './utils/thunder.js'
+import { redRobot, spring2 } from './utils/thunder.js'
 import select from '@inquirer/select'
 
 async function start() {
@@ -8,28 +8,12 @@ async function start() {
     message: '想做什麼呢',
     choices: [
       {
-        name: '奧迪溫',
-        value: '奧迪溫',
-      },
-      {
         name: '奧迪溫-左右',
         value: '奧迪溫-左右',
       },
       {
-        name: '奧迪溫-雅努斯',
-        value: '奧迪溫-雅努斯',
-      },
-      {
-        name: '桃源境2',
-        value: '桃源境2',
-      },
-      {
         name: '桃源境',
         value: '桃源境',
-      },
-      {
-        name: '奧迪溫實驗室',
-        value: '奧迪溫實驗室',
       },
       {
         name: 'money',
@@ -53,33 +37,18 @@ async function start() {
         value: '顯示座標',
       },
     ],
-  })
+  }).catch(() => null)
+  if (selectAnswer == null) return void console.log('使用者取消')
   console.log(`選了: ${selectAnswer}`)
 
   await beforeStart(3)
   switch (selectAnswer) {
-    case '奧迪溫':
-      redRobot()
-      break
-
     case '奧迪溫-左右':
       redRobot(true)
       break
 
-    case '奧迪溫-雅努斯':
-      奧迪溫雅努斯()
-      break
-
     case '桃源境':
-      spring()
-      break
-
-    case '桃源境2':
       spring2()
-      break
-
-    case '奧迪溫實驗室':
-      實驗室()
       break
 
     case 'money':
